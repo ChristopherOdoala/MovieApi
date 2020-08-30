8d25de522b66553c3ef047810278aeb18bcd8690
Christopher Odoala <codoala@sbsc.com>
2020-08-30 15:44:11 +0100

    Update

---
+++ MovieApi/Services/MovieService.cs
@@ -0,0 +1,29 @@
+﻿using MovieApi.Helpers;
+using MovieApi.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApi.Services
+{
+    public interface IMovieService
+    {
+
+    }
+    public class MovieService :IMovieService
+    {
+        private readonly DataContext _dataContext;
+        public MovieService(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public ResultModel<string> UploadMovie()
+        {
+            var resultModel = new ResultModel<string>();
+
+            return resultModel;
+        }
+    }
+}
---
+++ MovieApi/ViewModels/FileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApi.ViewModels
+{
+    public class FileViewModel
+    {
+        public Guid FileId { get; set; }
+        public string File { get; set; }
+        public string Name { get; set; }
+    }
+}
